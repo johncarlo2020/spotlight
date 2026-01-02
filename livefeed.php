1f0f61876d25870a9bcad1a676e6ae81c2fc4208
Your Name <you@example.com>
2026-01-02 15:18:14 +0000

    add new firework and lantern images; update livefeed to use webp format

--- livefeed.php
+++ livefeed.php
@@ -53,8 +53,8 @@ $latestImages = array_slice($images, 0, 18);
 <div class="animated-decorations">
     <img src="asset/lantern.gif" class="decoration lantern" alt="">
     <img src="asset/flower.gif" class="decoration flower" alt="">
-    <img src="asset/firework.gif" class="decoration firework-1" alt="">
-    <img src="asset/firework.gif" class="decoration firework-2" alt="">
+    <img src="asset/firework.webp" class="decoration firework-1" alt="">
+    <img src="asset/lantern.webp" class="decoration firework-2" alt="">
 </div>
 
 <div id="pixiCanvas"></div>
